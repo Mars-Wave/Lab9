af19e1a97aad0fa9ec8ef140dac5a622de02f83a
Tomás <tmoro@ucm.es>
2021-05-14 00:25:54 +0000

    master

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,11 @@ class BooksController < ApplicationController
       # If we said books = Book.all, we wouldn't be able to access books in the template
       @books = Book.paginate page: params[:page], per_page: 10
     end
+
+    def show
+     @book = Book.find(params[:id])
+     @comments = @book.comments.paginate(page: params[:page], per_page: 10).order('created_at ASC')
+    end
   end
+
+  
---
+++ app/models/comment.rb
@@ -0,0 +1,15 @@
+  
+  class Comment < ApplicationRecord
+    belongs_to :book
+  end
+  
+  class CreateComments < ActiveRecord::Migration[5.0]
+    def change
+      create_table :comments do |t|
+        t.text :text
+        t.string :author
+        t.belongs_to :book # creates a new integer column named book_id
+        t.timestamps
+      end
+    end
+  end
